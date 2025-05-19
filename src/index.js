ffc5da0994d6c042926de209ddb2867b552215ee
Yoon Hyungseok <yhsphd05@gmail.com>
2025-05-20 01:18:52 +0900

    Fix: Match program behavior with help text
    Use console.log before initializing logger with config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const yaml = require("js-yaml");
-const logger = require("winston");
 const parseArgs = require("minimist");
 
 const { initializeLogger } = require("./logger");
@@ -10,9 +9,8 @@ const { initializeOsuApi } = require("./osuApi");
 const { start } = require("./entry");
 const { mappoolgen } = require("./scripts/mappoolgen");
 
-var argv = parseArgs(process.argv.slice(2));
-
-const helpText = `
+const VERSION = "KDC Overlay v2024.2.0";
+const HELP = `
 kdc-overlay
 Duo Cup: KOREA 2025 Broadcasting Tool
 
@@ -24,56 +22,77 @@ Usage:
 
 Options:
   -h --help     Show this screen.
-  -v --version  Show version.`;
+  -v --version  Show version.
+`;
+
+const argv = parseArgs(process.argv.slice(2), {
+  alias: { h: "help", v: "version" },
+  boolean: ["help", "version"],
+});
 
 async function entry() {
-  if (argv?.h) {
-    console.log(helpText);
-  } else {
-    // Minimal Setup
-    const configFileExists = fs.existsSync(path.join(process.cwd(), "config.yaml"));
-    const streamConfigFileExists = fs.existsSync(path.join(process.cwd(), "config_stream.yaml"));
-    const firstRun = !(configFileExists && streamConfigFileExists);
-
-    if (firstRun) {
-      if (!configFileExists) {
-        await fs.copyFileSync(
-          path.join(__dirname, "templates/configs/config.default.yaml"),
-          path.join(process.cwd(), "config.yaml")
-        );
-        logger.info("Default config file created! Please re-run the program after you complete!");
-      }
-      if (!streamConfigFileExists) {
-        await fs.copyFileSync(
-          path.join(__dirname, "templates/configs/config_stream.default.yaml"),
-          path.join(process.cwd(), "config_stream.yaml")
-        );
-        logger.info(
-          "Default streamConfig file created! Please re-run the program after you complete!"
-        );
-      }
-      process.exit();
-    }
-
-    const config = yaml.load(
-      fs.readFileSync(path.join(process.cwd(), "config.yaml"), { encoding: "utf8", flag: "r" })
-    );
-
-    // initialize logger
-    initializeLogger(config.logLevel);
-
-    // osu!api (v2) init
-    await initializeOsuApi(config);
-
-    // End of initial setup
-
-    if (argv._[0] === "mappoolgen") {
-      mappoolgen();
-    } else {
-      // Main Entry
-      start(config);
-    }
+  // Help and version
+  if (argv.help) {
+    console.log(HELP);
+    process.exit(0);
   }
+
+  if (argv.version) {
+    console.log(VERSION);
+    process.exit(0);
+  }
+
+  const command = argv._[0];
+
+  // Error out when unknown command, should be updated when adding new commands
+  if (command && command !== "mappoolgen") {
+    console.error(`Unknown command: ${command}`);
+    console.log(HELP);
+    process.exit(1);
+  }
+
+  // Main setup (always runs before any command)
+  const configFilePath = path.join(process.cwd(), "config.yaml");
+  const streamConfigPath = path.join(process.cwd(), "config_stream.yaml");
+  const configFileExists = fs.existsSync(configFilePath);
+  const streamConfigFileExists = fs.existsSync(streamConfigPath);
+  const firstRun = !(configFileExists && streamConfigFileExists);
+
+  if (firstRun) {
+    if (!configFileExists) {
+      fs.copyFileSync(
+        path.join(__dirname, "templates/configs/config.default.yaml"),
+        configFilePath
+      );
+      console.log("Default config file created! Please re-run the program after you complete!");
+    }
+
+    if (!streamConfigFileExists) {
+      fs.copyFileSync(
+        path.join(__dirname, "templates/configs/config_stream.default.yaml"),
+        streamConfigPath
+      );
+      console.log(
+        "Default streamConfig file created! Please re-run the program after you complete!"
+      );
+    }
+
+    process.exit(0);
+  }
+
+  // We can load config
+  const config = yaml.load(fs.readFileSync(configFilePath, { encoding: "utf8", flag: "r" }));
+  initializeLogger(config.logLevel);
+  await initializeOsuApi(config);
+
+  // Mappoolgen script
+  if (command === "mappoolgen") {
+    mappoolgen();
+    return;
+  }
+
+  // Main entry point when no args or command
+  start(config);
 }
 
 entry();
